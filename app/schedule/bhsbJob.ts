8e61c383d019a2985bccfacd8549c8789e148b96
yanfang <braveoyster@gmail.com>
2022-01-05 18:07:29 +0800

    fix schedule job patch

--- app/schedule/bhsbJob.ts
+++ app/schedule/bhsbJob.ts
@@ -4,13 +4,21 @@ export default class BhsbJob extends Subscription {
   static get schedule() {
     return {
       cron: '* */15 16-18 * * *',
-      type: 'worker'
+      type: 'worker',
+      // interval: '5s',
+      // immediate: true
     };
   }
 
   async subscribe() {
+    const tmp = await this.service.redis.get('bhsbJob_tmp');
+    if (tmp && tmp.pendding) {
+      console.log(tmp);
+      return; // already has job running
+    }
+    await this.service.redis.set('bhsbJob_tmp', { pendding: true });
     this.ctx.logger.warn(`bhsbJob运行开始：${(new Date()).toString()}`);
-    await this.service.news.syncLatest();
+    // await this.service.news.syncLatest();
     this.ctx.logger.warn(`bhsbJob运行结束：${(new Date()).toString()}`);
   }
 }
