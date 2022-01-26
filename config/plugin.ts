0112f788bcdfe51bdd3e17c4c2b82fd2514424d2
yanfang <braveoyster@gmail.com>
2022-01-26 16:27:56 +0800

    fix cors

--- config/plugin.ts
+++ config/plugin.ts
@@ -6,6 +6,10 @@ const plugin: EggPlugin = {
   //   enable: true,
   //   package: 'egg-view-nunjucks',
   // },
+  cors: {
+    enable: true,
+    package: 'egg-cors',
+  },
   sequelize: {
     enable: true,
     package: 'egg-sequelize',
